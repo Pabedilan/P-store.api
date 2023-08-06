95e1bafb3bf903d2599f2eacee44b996d0985857
Pabedilan <114459299+Pabedilan@users.noreply.github.com>
2023-08-06 21:31:09 +0700

    Hendra Official

---
+++ controllers/message.js
@@ -0,0 +1,45 @@
+require("./settings")
+msg = {
+    paramkey: {
+    	status: "Success failed",
+    	code: 403,
+    	message: "Apikey parameters not found.",
+    	maintanied_by: author
+    },
+    paramquery: {
+    	status: "Success failed",
+    	code: 403,
+    	message: "Query parameters not found.",
+    	maintanied_by: author
+    },
+    paramtext: {
+    	status: "Success failed",
+    	code: 403,
+    	message: "Text parameters not found.",
+    	maintanied_by: author
+    },
+    paramtext2: {
+    	status: "Success failed",
+    	code: 403,
+    	message: "Text2 parameters not found.",
+    	maintanied_by: author
+    },
+    paramurl: {
+    	status: "Success failed",
+    	code: 403,
+    	message: "Url parameters not found.",
+    	maintanied_by: author
+    },
+    nodata: {
+    	status: "Error",
+    	code: 404,
+    	message: "Error data not Found.",
+    	maintanied_by: author
+    },
+    error: {
+    	status: "Error",
+    	code: 404,
+    	message: "Error not Found.",
+    	maintanied_by: author
+    }
+}
---
+++ controllers/settings.js
@@ -0,0 +1,12 @@
+global.author = "HendraOfficial";
+global.key_free = ["henfree"];
+global.key_premium = ["hendra"];
+global.limit_free = "100";
+global.limit_premium = "Unlimited";
+global.mongo_Db = "mongodb+srv://P-store:<password>@p-store.yyaopug.mongodb.net/?retryWrites=true&w=majority";
+global.recaptcha_key_1 = "6LcHhD4mAAAAABNrbkdrap9Gg9R_nVmksj4kcWbV";
+global.recaptcha_key_2 = "6LcHhD4mAAAAAEpdQpkfsY5D1Gf8k9UpBzjbm_ZO";
+global.my_email = "gvgameindonesia@gmail.com";
+global.my_email_password = "Budeaxasb13";
+global.activation_token = "hendra";
+global.port = 3000
